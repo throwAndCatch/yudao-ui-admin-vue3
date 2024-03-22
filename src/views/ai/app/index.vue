490891f0602369599de03fa0455950e9a1218012
throwAndCatch <programmer.zhoup@gmail.com>
2024-03-22 10:03:05 +0800

    add sth

--- src/views/ai/app/index.vue
+++ src/views/ai/app/index.vue
@@ -17,115 +17,6 @@
           class="!w-240px"
         />
       </el-form-item>
-      <el-form-item label="头像地址" prop="avatar">
-        <el-input
-          v-model="queryParams.avatar"
-          placeholder="请输入头像地址"
-          clearable
-          @keyup.enter="handleQuery"
-          class="!w-240px"
-        />
-      </el-form-item>
-      <el-form-item label="用户昵称" prop="nickname">
-        <el-input
-          v-model="queryParams.nickname"
-          placeholder="请输入用户昵称"
-          clearable
-          @keyup.enter="handleQuery"
-          class="!w-240px"
-        />
-      </el-form-item>
-      <el-form-item label="开场介绍" prop="prologue">
-        <el-input
-          v-model="queryParams.prologue"
-          placeholder="请输入开场介绍"
-          clearable
-          @keyup.enter="handleQuery"
-          class="!w-240px"
-        />
-      </el-form-item>
-      <el-form-item label="相似度" prop="similarity">
-        <el-input
-          v-model="queryParams.similarity"
-          placeholder="请输入相似度"
-          clearable
-          @keyup.enter="handleQuery"
-          class="!w-240px"
-        />
-      </el-form-item>
-      <el-form-item label="单词检索条数" prop="retrievedEntries">
-        <el-input
-          v-model="queryParams.retrievedEntries"
-          placeholder="请输入单词检索条数"
-          clearable
-          @keyup.enter="handleQuery"
-          class="!w-240px"
-        />
-      </el-form-item>
-      <el-form-item label="未命中策略 0--默认知识库  1--固定文案" prop="missedPolicies">
-        <el-input
-          v-model="queryParams.missedPolicies"
-          placeholder="请输入未命中策略 0--默认知识库  1--固定文案"
-          clearable
-          @keyup.enter="handleQuery"
-          class="!w-240px"
-        />
-      </el-form-item>
-      <el-form-item label="固定文案" prop="fixedCopy">
-        <el-input
-          v-model="queryParams.fixedCopy"
-          placeholder="请输入固定文案"
-          clearable
-          @keyup.enter="handleQuery"
-          class="!w-240px"
-        />
-      </el-form-item>
-      <el-form-item label="知识库类型，字典值" prop="knowledgeBaseType">
-        <el-select
-          v-model="queryParams.knowledgeBaseType"
-          placeholder="请选择知识库类型，字典值"
-          clearable
-          class="!w-240px"
-        >
-          <el-option label="请选择字典生成" value="" />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="记忆" prop="memory">
-        <el-input
-          v-model="queryParams.memory"
-          placeholder="请输入记忆"
-          clearable
-          @keyup.enter="handleQuery"
-          class="!w-240px"
-        />
-      </el-form-item>
-      <el-form-item label="温度" prop="temperature">
-        <el-input
-          v-model="queryParams.temperature"
-          placeholder="请输入温度"
-          clearable
-          @keyup.enter="handleQuery"
-          class="!w-240px"
-        />
-      </el-form-item>
-      <el-form-item label="应用设定" prop="appSetting">
-        <el-input
-          v-model="queryParams.appSetting"
-          placeholder="请输入应用设定"
-          clearable
-          @keyup.enter="handleQuery"
-          class="!w-240px"
-        />
-      </el-form-item>
-      <el-form-item label="链接" prop="link">
-        <el-input
-          v-model="queryParams.link"
-          placeholder="请输入链接"
-          clearable
-          @keyup.enter="handleQuery"
-          class="!w-240px"
-        />
-      </el-form-item>
       <el-form-item label="创建时间" prop="createTime">
         <el-date-picker
           v-model="queryParams.createTime"
@@ -161,52 +52,35 @@
     </el-form>
   </ContentWrap>
 
-  <!-- 列表 -->
   <ContentWrap>
-    <el-table v-loading="loading" :data="list" :stripe="true" :show-overflow-tooltip="true">
-      <el-table-column label="自增主键" align="center" prop="id" />
-      <el-table-column label="应用名称" align="center" prop="name" />
-      <el-table-column label="头像地址" align="center" prop="avatar" />
-      <el-table-column label="用户昵称" align="center" prop="nickname" />
-      <el-table-column label="描述" align="center" prop="description" />
-      <el-table-column label="开场介绍" align="center" prop="prologue" />
-      <el-table-column label="相似度" align="center" prop="similarity" />
-      <el-table-column label="单词检索条数" align="center" prop="retrievedEntries" />
-      <el-table-column label="未命中策略 0--默认知识库  1--固定文案" align="center" prop="missedPolicies" />
-      <el-table-column label="固定文案" align="center" prop="fixedCopy" />
-      <el-table-column label="知识库类型，字典值" align="center" prop="knowledgeBaseType" />
-      <el-table-column label="记忆" align="center" prop="memory" />
-      <el-table-column label="温度" align="center" prop="temperature" />
-      <el-table-column label="应用设定" align="center" prop="appSetting" />
-      <el-table-column label="链接" align="center" prop="link" />
-      <el-table-column
-        label="创建时间"
-        align="center"
-        prop="createTime"
-        :formatter="dateFormatter"
-        width="180px"
-      />
-      <el-table-column label="操作" align="center">
-        <template #default="scope">
-          <el-button
-            link
-            type="primary"
-            @click="openForm('update', scope.row.id)"
-            v-hasPermi="['ai:app:update']"
-          >
-            编辑
-          </el-button>
-          <el-button
-            link
-            type="danger"
-            @click="handleDelete(scope.row.id)"
-            v-hasPermi="['ai:app:delete']"
-          >
-            删除
-          </el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+    <el-card style="max-width: 480px" shadow="hover" v-for="obj  in list"  :key="obj.id">
+      <template #header>
+        <div class="card-header">
+          <span>{{obj.id}}</span>
+        </div>
+      </template>
+      <p  class="text item">{{obj.name}}</p>
+      <template #footer="scope">
+
+        <el-button
+          link
+          type="primary"
+          @click="openForm('update', scope.row.id)"
+          v-hasPermi="['ai:app:update']"
+        >
+          编辑
+        </el-button>
+        <el-button
+          link
+          type="danger"
+          @click="handleDelete(scope.row.id)"
+          v-hasPermi="['ai:app:delete']"
+        >
+          删除
+        </el-button>
+
+      </template>
+    </el-card>
     <!-- 分页 -->
     <Pagination
       :total="total"
